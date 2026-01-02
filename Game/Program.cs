ccfebe2bffb9feccf49aa72f5d6e350527d120e9
Csump <60896824+Csump@users.noreply.github.com>
2026-01-02 18:39:17 +0100

    Fix status update

--- Game/Program.cs
+++ Game/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddRazorComponents()
 builder.Services.AddDbContext<GameDbContext>(options =>
     options.UseInMemoryDatabase("GameDb"));
 builder.Services.AddScoped<GameService>();
-builder.Services.AddSingleton<GameState>();
+builder.Services.AddScoped<GameState>();
 
 var app = builder.Build();
 
