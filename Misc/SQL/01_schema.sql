11a2578319e85c4137d5685a83d7d2751101a35b
Csump <60896824+Csump@users.noreply.github.com>
2026-01-23 13:00:11 +0100

    Update 01_schema.sql

--- Misc/SQL/01_schema.sql
+++ Misc/SQL/01_schema.sql
@@ -3,6 +3,7 @@ BEGIN;
 drop type if exists situation_type_enum;
 
 CREATE TYPE situation_type_enum AS ENUM ('Info', 'Decision', 'Minigame', 'Conversation', 'Special');
+CREATE TYPE player_level AS ENUM ('Doktorandusz', 'Docens', 'Adjunktus');
 
 drop table if exists choices;
 drop table if exists situations;
@@ -31,6 +32,29 @@ CREATE TABLE choices (
     delta_creativity INTEGER,
     delta_cooperation INTEGER,
     delta_motivation INTEGER
+);
+
+CREATE TABLE players (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    is_male BOOLEAN,
+    is_old BOOLEAN,
+    level player_level,
+    energy INTEGER DEFAULT 0,
+    selfreflection INTEGER DEFAULT 0,
+    competency INTEGER DEFAULT 0,
+    initiative INTEGER DEFAULT 0,
+    creativity INTEGER DEFAULT 0,
+    cooperation INTEGER DEFAULT 0,
+    motivation INTEGER DEFAULT 0,
+    current_situation_id INTEGER REFERENCES situations(id)
+);
+
+CREATE TABLE decisions (
+    id SERIAL PRIMARY KEY,
+    player_id INTEGER REFERENCES players(id),
+    choice_id INTEGER REFERENCES choices(id),
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 COMMIT;
