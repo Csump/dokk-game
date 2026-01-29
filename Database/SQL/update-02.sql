1f109742283a201cfc570ff014b69516eaa8819d
Csump <60896824+Csump@users.noreply.github.com>
2026-01-29 19:21:30 +0100

    Create update-02.sql

---
+++ Database/SQL/update-02.sql
@@ -0,0 +1,9 @@
+UPDATE situations
+SET situation_text = E'Az egyetemi OMHV rendszerben összegzett adatok az online kvíz módszert alkalmazó kurzusokról. Az előző három tanév átlaga vs. a módszer bevezetése után:\n\
+– Az oktató módszerével elégedett: 53% / 34%.\n\
+– Mennyire volt interaktív a foglalkozás: 13% / 21%.\n\
+– A kurzus oktatóját ajánlaná-e hallgatótársainak: 69% / 61%.\n\
+Szöveges válaszok:\n\
+Az OMHV rendszerben rögzített szöveges válaszok szerint az online kvíz módszert a hallgatók többsége kezdetben pozitívan fogadta, értékelve annak játékos elemeit. Később azonban előjöttek a korlátok is. A kvíz állandó ismételgetése minden órán előbb unalmassá, később pedig sokak számára terhessé vált: „Hiába mások a kérdések, tök unalmas mindig ugyanazt a játékot gyúrni.” Előfordult, hogy az internet kapcsolat nem volt elég stabil az online kvíz zökkenőmentes lejátszásához, vagy más technikai probléma lépett fel, így az a tervezettnél sokkal több időt vett igénybe: „Kínos volt a hiba hosszas keresgélése, miközben rengetek dolgunk lett volna. Amatőrök.”\n\
+A hallgatók visszajelzései szerint az eszköz nem alkalmas komplexebb kérdések megfogalmazására, így a tananyag lényeges területei a kvíz számára érintetlenek maradtak: „A kvízből keveset tanulunk, mert képtelen az alapfogalmaknál mélyebbre menni.” Egyeseket zavart az is, hogy a válaszadás gyorsasága is fontos a játékban, mivel az ilyenfajta gyorsaság nem kiemelt szakmai követelmény: „Idegesít az időmérés, nem szeretek kapkodni, stresszel. Szerintem ez nem lóverseny. Vagy mégis?”'
+WHERE id = 2;
