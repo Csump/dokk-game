3c3de6bb9142539ede7de179ac59c6e83ab5f3a9
Pongracz Rita Csilla <por7bp@bosch.com>
2026-01-29 17:22:22 +0100

    Update 04_constraints.sql

--- Database/SQL/04_constraints.sql
+++ Database/SQL/04_constraints.sql
@@ -3,22 +3,6 @@ ALTER TABLE situations
 ADD CONSTRAINT fk_situations_next 
 FOREIGN KEY (next_situation_id) REFERENCES situations(id);
 
-ALTER TABLE choices 
-ADD CONSTRAINT fk_choices_next 
-FOREIGN KEY (next_situation_id) REFERENCES situations(id);
-
-ALTER TABLE choices 
-ADD CONSTRAINT fk_choices_situation 
-FOREIGN KEY (situation_id) REFERENCES situations(id);
-
-ALTER TABLE decisions 
-ADD CONSTRAINT fk_decisions_player 
-FOREIGN KEY (player_id) REFERENCES players(id);
-
-ALTER TABLE decisions 
-ADD CONSTRAINT fk_decisions_choice 
-FOREIGN KEY (choice_id) REFERENCES choices(id);
-
 -- Fix sequences
 CREATE SEQUENCE situations_id_seq;
 ALTER TABLE situations ALTER COLUMN id SET DEFAULT nextval('situations_id_seq');
