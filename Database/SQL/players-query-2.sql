2fb643265c2d893a6349b2a62bb1d57ebb77d823
Csump <60896824+Csump@users.noreply.github.com>
2026-01-30 17:41:54 +0100

    Create players-query-2.sql

---
+++ Database/SQL/players-query-2.sql
@@ -0,0 +1,8 @@
+SELECT
+  name,
+  to_char(created_at, 'YYYY. MM. DD. HH24:MI') AS created_at,
+  to_char(completed_at, 'YYYY. MM. DD. HH24:MI') AS completed_at,
+  (energy + selfreflection + competency + initiative + creativity + cooperation + motivation) AS score_sum,
+  to_char(completed_at - created_at, 'HH24:MI:SS') AS completion_time
+FROM players
+ORDER BY created_at DESC;
