b969e8f1c1ff87120fbae3c3eae7ba6c70be2cec
Csump <60896824+Csump@users.noreply.github.com>
2026-02-22 19:57:46 +0100

    Update add-links.sql

--- 2.0 Release/add-links.sql
+++ 2.0 Release/add-links.sql
@@ -1,8 +1,14 @@
+BEGIN;
+
 ALTER TABLE choices 
 ADD COLUMN link TEXT NULL;
 
-UPDATE choices 
-SET link = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' 
-WHERE id IN (24, 25, 26, 27, 28);
+UPDATE choices
+SET link = CASE
+    WHEN id = 27 THEN 'https://docs.google.com/document/d/15A_PVxn_7HKL632M-fFX0C4zs1V1yA1T/'
+    WHEN id = 28 THEN 'https://docs.google.com/document/d/1FE5itZB2WxcklbyspArCzLyYgU2SZEGS/'
+    ELSE link
+END
+WHERE id IN (27, 28);
 
 COMMIT;
