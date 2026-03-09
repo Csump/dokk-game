e53c6a8574e7fb0b5f99a665a37a37e37f262207
Csump <60896824+Csump@users.noreply.github.com>
2026-03-09 14:35:51 +0100

    Create db-to-csv.sql

---
+++ Database/SQL/db-to-csv.sql
@@ -0,0 +1,4 @@
+\copy players    TO '/tmp/scripts/players.csv'    CSV HEADER DELIMITER ';' ENCODING 'UTF8';
+\copy decisions  TO '/tmp/scripts/decisions.csv'  CSV HEADER DELIMITER ';' ENCODING 'UTF8';
+\copy situations TO '/tmp/scripts/situations.csv' CSV HEADER DELIMITER ';' ENCODING 'UTF8';
+\copy choices    TO '/tmp/scripts/choices.csv'    CSV HEADER DELIMITER ';' ENCODING 'UTF8';
